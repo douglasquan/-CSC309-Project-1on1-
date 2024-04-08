19c640ea8cc3906232910995a32963acf7145201
Wayne Yeung <waynemyeung@gmail.com>
2024-04-08 03:29:54 -0400

    Fixed Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import "./styles/index.css";
 import './styles/output.css'; 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import PrivateRoute from './utils/PrivateRoute'
 import { AuthProvider } from './context/AuthContext'
 
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
-import NavbarMenu from "./components/Navbar";
+import RegisterPage from './pages/RegisterPage';
 import CreateEventPage from "./pages/CreateEventPage";
 import ContactListPage from "./pages/ContactListPage"
 import ProfilePage from "./pages/ProfilePage";
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import LayoutWithNavbar from "./components/LayoutWithNavbar";
+
 
 function App() {
   return (
@@ -22,12 +23,16 @@ function App() {
             <Route path="/login" component={LoginPage} />
             <Route path="/register" component={RegisterPage} />
             <Route path="/forgot-password" component={ForgotPasswordPage} />
-            <LayoutWithNavbar>
-              <PrivateRoute component={HomePage} path="/" exact />
-              <PrivateRoute component={ProfilePage} path="/profile" exact/>
-              <PrivateRoute component={CreateEventPage} path="/create-event" exact />
-              <PrivateRoute component={ContactListPage} path="/contact-list" exact />
-            </LayoutWithNavbar>
+            <Route path="/" render={() => (
+              <LayoutWithNavbar>
+                <Switch>
+                  <PrivateRoute exact path="/" component={HomePage} />
+                  <PrivateRoute exact path="/profile" component={ProfilePage} />
+                  <PrivateRoute exact path="/create-event" component={CreateEventPage} />
+                  <PrivateRoute exact path="/contact-list" component={ContactListPage} />
+                </Switch>
+              </LayoutWithNavbar>
+            )} />
           </Switch>
         </AuthProvider>
       </Router>
